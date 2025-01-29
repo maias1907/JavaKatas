d94202f2b45d8961fe1bd29a065aed8b2f4b4688
maias1907 <144439334+maias1907@users.noreply.github.com>
2025-01-30 00:03:27 +0200

    Kth smallesst

--- src/test/java/KthSmallestElementinBSTTest.java
+++ src/test/java/KthSmallestElementinBSTTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static katas.exercises.KthSmallestElementInBST.kthSmallest;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class KthSmallestElementInBSTTest {
+public class KthSmallestElementinBSTTest {
 
     @Test
     public void testKthSmallestBalancedTree() {
