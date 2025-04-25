f553ff2b6286a3bfed85de52f23a440ed5c6ee42
maias <maias9018@gmail.com>
2025-04-25 15:34:58 +0300

    wallet

---
+++ src/main/java/ArraysAndStrings/SpiralMatrix.java
@@ -0,0 +1,62 @@
+package ArraysAndStrings;
+//Given an m x n matrix, return all elements of the matrix in spiral order.
+//
+//
+//
+//Example 1:
+//
+//
+//Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+//Output: [1,2,3,6,9,8,7,4,5]
+//Example 2:
+//
+//
+//Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+//Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) return result;
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            // Move right
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            // Move down
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            // Move left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Move up
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
